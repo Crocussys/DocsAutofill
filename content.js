681421e2086857adf882f59825fa27249c159387
Crocussys <egor.vladislavovich.nn@gmail.com>
2026-03-06 17:50:21 +0300

    ЧЗ changed?

--- content.js
+++ content.js
@@ -106,12 +106,13 @@ function getFillDatesButton() {
 }
 
 function addButton(btn) {
-    const autocompleteRoot = document.querySelector('div.MuiAutocomplete-root[productgroupids="15"][documenttypecode="231"]');
-    if (!autocompleteRoot || document.getElementById(btn.id)) return;
-
-    const container = autocompleteRoot.parentElement.parentElement;
+    const inscription = Array.from(document.querySelectorAll('h3')).filter(h => h.innerText === 'Список кодов');
+    const container = inscription.parentElement.querySelector('.MuiStack-root');
     const otherButton = container.getElementsByTagName('button')[0];
-    if (!otherButton) return;
+    if (!otherButton) {
+        console.warn('[DateAutofill] Кнопки не найдены')
+        return;
+    }
 
     container.appendChild(btn);
 
